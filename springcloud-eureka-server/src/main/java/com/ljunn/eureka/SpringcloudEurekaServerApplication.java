489abfdaf63c3f9f3a48a79cc2b1369d605f9bbf
Li Jun LM Peng <pengljun@cn.ibm.com>
2021-09-08 15:39:26 +0800

    Initial commit

---
+++ springcloud-eureka-server/src/main/java/com/ljunn/eureka/SpringcloudEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ljunn.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEurekaServerApplication.class, args);
+    }
+
+}
